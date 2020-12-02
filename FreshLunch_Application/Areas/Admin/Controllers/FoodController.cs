bb5e40666a48d63724017114812353093d051edc
Vidhyadhar Galande <vidhyadhar.galande@outlook.com>
2020-12-02 12:44:06 +0530

    Add project files.

---
+++ FreshLunch_Application/Areas/Admin/Controllers/FoodController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FreshLunch_Application.Areas.Admin.Controllers
+{
+    public class FoodController : Controller
+    {
+        // GET: Admin/Food
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddFood()
+        {
+            return View();
+        }
+    }
+}
---
+++ FreshLunch_Application/Areas/Admin/Controllers/SchoolController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FreshLunch_Application.Areas.Admin.Controllers
+{
+    public class SchoolController : Controller
+    {
+        // GET: Admin/School
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
